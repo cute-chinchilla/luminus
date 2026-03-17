a1d4f34d67eb64d025fa689ea79b89dd48804317
jihye <clsrndnflsms@gmail.com>
2026-03-18 00:27:50 +0900

    chore: update build client path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
   //   server: './dist/_worker.js',
   // },
   build: {
-    client: './dist/client', // 정적 파일 (이미지, JS 등)
+    client: './dist', // 정적 파일 (이미지, JS 등)
     server: './dist'         // 서버 파일 (_worker.js가 여기 생김)
   },
   integrations: [
