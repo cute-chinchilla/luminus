7ac4890cc861819a49dd81513abc4c05b73cd005
jihye <clsrndnflsms@gmail.com>
2026-03-17 20:40:50 +0900

    fix: revert to standard cloudflare configuration to fix 404 SSR issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,7 @@ import clerk from '@clerk/astro';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  adapter: cloudflare(),
   integrations: [
     preact({ compat: true }),
     clerk()
