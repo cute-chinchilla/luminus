7d9f86d3679464366ca14e1ace688b5b54eba530
jihye <clsrndnflsms@gmail.com>
2026-03-17 23:48:57 +0900

    chore: comment out explicit build output paths in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,10 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: 'passthrough',
   }),
-  build: {
-    client: './dist',
-    server: './dist/_worker.js',
-  },
+  // build: {
+  //   client: './dist',
+  //   server: './dist/_worker.js',
+  // },
   integrations: [
     preact({ compat: true }),
     clerk()
